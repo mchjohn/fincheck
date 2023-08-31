9743a0b16d13ee24d5eadac2c566e90e34a4b002
Michel John <michel.john@hotmail.com>
2023-08-31 13:10:16 -0300

    add readme and fix table name

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Fincheck aplicação para gerênciamento de finanças
+
+Aplicação construida no evento JStack do Matheus Silva
+
+- [ERD da aplicação](https://lucid.app/lucidchart/c35329ca-57ed-47f9-b878-47ece745c683/edit?viewport_loc=-132%2C-24%2C1623%2C1111%2CZvKdXZheBP8n&invitationId=inv_8f1a4582-ba0b-485b-bc34-d994f6778caf)
+
+
+## Stack utilizada
+
+**Front-end:** React
+
+**Back-end:** NestJs, Postgres, Prisma, Docker
--- api/prisma/migrations/20230831160202_create_all_tables/migration.sql
+++ api/prisma/migrations/20230831160202_create_all_tables/migration.sql
@@ -1,14 +1,30 @@
-/*
-  Warnings:
+-- CreateEnum
+CREATE TYPE "bank_account_type" AS ENUM ('CHECKING', 'INVESTIMENT', 'CASH');
 
-  - You are about to drop the `Category` table. If the table is not empty, all the data it contains will be lost.
+-- CreateEnum
+CREATE TYPE "transaction_type" AS ENUM ('INCOME', 'EXPENSE');
 
-*/
--- DropForeignKey
-ALTER TABLE "Category" DROP CONSTRAINT "Category_user_id_fkey";
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
 
--- DropTable
-DROP TABLE "Category";
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bank_accounts" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "initial_balance" DOUBLE PRECISION NOT NULL,
+    "type" "bank_account_type" NOT NULL,
+    "color" TEXT NOT NULL,
+
+    CONSTRAINT "bank_accounts_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "categories" (
@@ -34,6 +50,12 @@ CREATE TABLE "transactions" (
 
     CONSTRAINT "transactions_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "bank_accounts" ADD CONSTRAINT "bank_accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "categories" ADD CONSTRAINT "categories_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
